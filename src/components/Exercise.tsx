8d06506bbc1753de8c0567d3f31740a4a84271a5
Ruth Vargas <vargasmruth@gmail.com>
2024-01-17 01:13:09 -0500

    Add exercise and exercises pages

---
+++ src/components/Exercise.tsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import { connectDB } from "./db/db";
+import { getExercise } from "./db/queries";
+
+const db = connectDB();
+
+export const Exercise = () => {
+  const [exercise, setExercise] = useState<any>(null);
+
+  useEffect(() => {
+    const updateExercise = async () => {
+      const id = new URL(window.location.href).searchParams.get("id");
+      if (!id) return;
+      const exercise = await getExercise(id);
+      setExercise(exercise);
+    };
+    updateExercise();
+  }, []);
+
+  if (!exercise) {
+    return null;
+  }
+  return (
+    <>
+      {exercise.ThumbnailUrl && (
+        <img src={exercise.ThumbnailUrl} alt={exercise.ExerciseName} />
+      )}
+
+      <div className="item-content">
+        <h2>{exercise.ExerciseName}</h2>
+        {/* Insert rendered description (md -> html) */}
+        <h4>Muscle Groups</h4>
+        <ul>
+          {exercise.MuscleGroups.map((muscleGroup) => (
+            <a href={`/muscleGroup/${muscleGroup.id}`}>{muscleGroup.name}</a>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
--- src/components/ExerciseList.tsx
+++ src/components/ExerciseList.tsx
@@ -1,20 +1,50 @@
 import React, { useState, useEffect } from "react";
+import { connectDB } from "./db/db";
+import { getExercises } from "./db/queries";
+import "./ExerciseList.css";
+
+const db = connectDB();
 
 export const ExerciseList = () => {
-  const [exercises, setExercises] = useState([]);
+  const [exercises, setExercises] = useState<any>([]);
 
   useEffect(() => {
-    // TODO: Query sqlite db for exercises (view?)
-    // Query all exercises.
+    const awaitExercises = async () => {
+      const exercises = await getExercises();
+      setExercises(exercises);
+    };
+
+    awaitExercises();
   }, []);
 
   return (
     <>
       <h1>Exercises</h1>
-      {exercises.map((exercise) => (
-        <>
-          <p>Todo</p>
-        </>
+      {exercises.map((exercise, idx) => (
+        <div className="list-wrapper" key={idx}>
+          <ul className="list">
+            <li className="list-item">
+              {exercise.ThumbnailUrl && (
+                <img src={exercise.ThumbnailUrl} alt={exercise.ExerciseName} />
+              )}
+
+              <div className="item-content">
+                <a href={`/exercise?id=${exercise.ExerciseID}`}>
+                  <h2>{exercise.ExerciseName}</h2>
+                </a>
+                {/* Insert rendered description (md -> html) */}
+                <h4>Muscle Groups</h4>
+                <ul>
+                  {exercise.MuscleGroups.map((muscleGroup, idx) => (
+                    <a href={`/muscleGroup/${muscleGroup.id}`} key={idx}>
+                      {muscleGroup.name}
+                    </a>
+                  ))}
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </div>
       ))}
     </>
   );
