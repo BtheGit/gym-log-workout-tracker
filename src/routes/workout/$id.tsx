00338edd6b930f19e4fadd09ef824b736003f295
Brendan Beltz <brendanwebdev@gmail.com>
2024-02-02 11:36:54 -0500

    Ensure set values are rendered when 0

--- src/routes/workout/$id.tsx
+++ src/routes/workout/$id.tsx
@@ -20,18 +20,20 @@ export function component() {
       <div>
         <h2>Exercises</h2>
         <ul>
-          {workout.exercises?.length &&
+          {workout.exercises?.length > 0 &&
             workout.exercises.map((exercise) => (
               <li key={exercise.workout_exercise_id}>
                 <h4>{exercise.exercise_name}</h4>
                 <ul>
-                  {exercise.sets?.length &&
+                  {exercise.sets?.length > 0 &&
                     exercise.sets.map((set) => (
                       <li key={set.set_id}>
-                        {set.reps && <p>Reps: {set.reps}</p>}
-                        {set.weight && <p>Weight: {set.weight}</p>}
-                        {set.distance && <p>Distance: {set.distance}</p>}
-                        {set.time && <p>Time: {set.time}</p>}
+                        {set.reps != null && <p>Reps: {set.reps}</p>}
+                        {set.weight != null && <p>Weight: {set.weight}</p>}
+                        {set.distance != null && (
+                          <p>Distance: {set.distance}</p>
+                        )}
+                        {set.time != null && <p>Time: {set.time}</p>}
                       </li>
                     ))}
                 </ul>
