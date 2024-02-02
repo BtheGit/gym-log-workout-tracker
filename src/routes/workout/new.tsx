beae1626794ee961ab9afae04c2df23b6af4624b
Brendan Beltz <brendanwebdev@gmail.com>
2024-02-02 13:31:10 -0500

    Type optional search params for new workout

--- src/routes/workout/new.tsx
+++ src/routes/workout/new.tsx
@@ -1,15 +1,27 @@
-import { FileRoute } from "@tanstack/react-router";
-import { WorkoutEditor } from "../../components/ProgramEditor/WorkoutEditor";
+import { FileRoute, useSearch } from "@tanstack/react-router";
+import { WorkoutEditor } from "../../components/TemplateEditors/WorkoutEditor";
+
+type IWorkoutEditorSearchParams = {
+  programId: string | undefined;
+};
 
 export const Route = new FileRoute("/workout/new").createRoute({
   component,
+  validateSearch: (
+    search: Record<string, unknown>
+  ): IWorkoutEditorSearchParams => {
+    return {
+      programId: (search.programId as string) ?? undefined,
+    };
+  },
 });
 
 export function component() {
+  const { programId } = useSearch({ from: "/workout/new" });
   return (
     <>
       <h1>New Workout</h1>
-      <WorkoutEditor />
+      <WorkoutEditor programId={programId} />
     </>
   );
 }
